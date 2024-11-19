a93474ce7a3b84c410bb80a0782468374ddc9dd2
samchencode <samchencode@gmail.com>
2024-11-19 06:17:23 -0600

    build: bump android version

--- app.config.js
+++ app.config.js
@@ -25,7 +25,7 @@ module.exports = {
         backgroundColor: '#000000',
       },
       package: 'com.strokemgmtapp.strokemgmt',
-      versionCode: 3,
+      versionCode: 4,
     },
     androidNavigationBar: {
       barStyle: 'dark-content',
